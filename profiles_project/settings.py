0c3de94be9e1ce66972fb2b3140e63d46477ad8d
Michalczyk <dawid.michalczyk@capgemini.com>
2022-03-13 14:48:16 +0100

    Running server

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_auth_token',
-    'profiles_api'
+    'rest_framework.authtoken',
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
